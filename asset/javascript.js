71666c739eb2b9880386935bb6f1abc2134213a0
Jeiner <jeinerarenas435@gmail.com>
2025-07-29 09:06:31 -0500

    guardar cambios en landing pages

---
+++ asset/javascript.js
@@ -0,0 +1,39 @@
+const track = document.getElementById('carouselTrack');
+  const prevBtn = document.querySelector('.carousel-btn.prev');
+  const nextBtn = document.querySelector('.carousel-btn.next');
+  let index = 0;
+  let visibleCards = 4;
+
+  function updateVisibleCards() {
+    if (window.innerWidth < 576) visibleCards = 1;
+    else if (window.innerWidth < 992) visibleCards = 2;
+    else visibleCards = 4;
+  }
+
+  function slideTo(index) {
+    const cardWidth = track.querySelector('.carousel-card').offsetWidth + 20;
+    track.style.transform = `translateX(-${index * cardWidth}px)`;
+  }
+
+  function slideNext() {
+    const totalCards = track.querySelectorAll('.carousel-card').length;
+    console.log(totalCards);
+    updateVisibleCards();
+    index++;
+    if (index > totalCards - visibleCards) index = 0;
+    slideTo(index);
+  }
+
+  function slidePrev() {
+    const totalCards = track.querySelectorAll('.carousel-card').length;
+    updateVisibleCards();
+    index--;
+    if (index < 0) index = totalCards - visibleCards;
+    slideTo(index);
+  }
+
+  nextBtn.addEventListener('click', slideNext);
+  prevBtn.addEventListener('click', slidePrev);
+  window.addEventListener('resize', updateVisibleCards);
+
+//   setInterval(slideNext, 5000); // Auto deslizamiento cada 1 segundo
